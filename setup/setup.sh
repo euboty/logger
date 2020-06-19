5ab2b5f3f5b796179b579f9b0309f3d2b01f2d12
Sergej Herbert <herbert.sergej@gmail.com>
2020-06-19 07:57:44 +0200

    Fix: Correct modules for setup.sh
    the temperature sensor modules were written to the wrong file
    and the gyroscope (i2c) modules were added

--- setup/setup.sh
+++ setup/setup.sh
@@ -6,13 +6,23 @@ set -x
 apt-get update
 apt-get install -y nginx sqlite3 python3-pip
 
+# enable i2c for gyroscope
+modprobe i2c_bcm2835
+modprobe i2c_dev
+echo "" >> /etc/modules
+echo "# vibration sensor config" >> /boot/config.txt
+echo "i2c_bcm2835" >> /etc/modules
+echo "i2c_dev" >> /etc/modules
+
 # enable temperature sensor on PIN 4
 modprobe wire
 modprobe w1-gpio
 modprobe w1-therm
+echo "" >> /etc/modules
 echo "# temperature sensor config" >> /boot/config.txt
-echo "w1-gpio" >> /boot/config.txt
-echo "w1-therm" >> /boot/config.txt
+echo "wire" >> /etc/modules
+echo "w1-gpio" >> /etc/modules
+echo "w1-therm" >> /etc/modules
 
 echo "" >> /boot/config.txt
 echo "# temperature sensor config" >> /boot/config.txt
